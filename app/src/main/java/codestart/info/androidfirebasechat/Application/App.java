d95600b787c817b9afa344ef6274e1b3974a7cd3
Mehul Sharma <cs17mtech11020@iith.ac.in>
2018-06-13 12:44:37 +0530

    Initial commit

---
+++ app/src/main/java/codestart/info/androidfirebasechat/Application/App.java
@@ -0,0 +1,16 @@
+package codestart.info.androidfirebasechat.Application;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //set database to persist
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
